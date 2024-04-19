0c1579f939499665f218156269007698c1a66dca
YagoLisboa <yago.lisboa@live.com>
2024-04-19 00:57:30 -0300

    Variáveis e constantes

---
+++ 2 - variaveis_constantes.py
@@ -0,0 +1,12 @@
+name = "Yago"
+age = 28
+
+name = "José"
+
+print(name, age, "anos")
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["MA", "CE", "BA", "PE", "RN", "PB", "PI"]
+
+print(BRAZILIAN_STATES)
