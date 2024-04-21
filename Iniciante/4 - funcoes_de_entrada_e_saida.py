3897002407dbc3c736abfc1bff580d68eff4904f
YagoLisboa <yago.lisboa@live.com>
2024-04-21 03:35:37 -0300

    funções de entrada e saída

---
+++ Iniciante/4 - funcoes_de_entrada_e_saida.py
@@ -0,0 +1,9 @@
+# entrada de dados - interação com o usuário
+name = input("Digite o seu nome: ")
+age = input("Digite a sua idade: ")
+
+# saída de dados e suas possibilidades
+print(name, age)
+print(name, age, end="...\n")
+print(name, age, sep="#")
+print(name, age, sep="@", end="#\n")
