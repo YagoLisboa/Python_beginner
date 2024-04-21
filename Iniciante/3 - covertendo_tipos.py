c6fb0e87eaf0bd930cab073e60419509931cfc7f
YagoLisboa <yago.lisboa@live.com>
2024-04-21 03:17:18 -0300

    convertendo tipos de dados primitivos

---
+++ Iniciante/3 - covertendo_tipos.py
@@ -0,0 +1,25 @@
+# convertendo em inteiro
+print(int(1.9515487))
+print(int("10"))
+
+# convertendo em ponto flutuante
+print(float("10"))
+print(float(15.369175))
+
+# convertendo em ponto string
+print(str(10.10))
+print(str(1.9515487))
+
+# conferindo os tipos a partir das conversões
+valor = 10
+valor_str = str(valor)
+valor_float = float(valor)
+print(type(valor))
+print(type(valor_str))
+print(type(valor_float))
+
+# divissão que resulta em ponto flutuante
+print(100 / 5)
+
+# divissão que resulta em inteiro
+print(100 // 5)
