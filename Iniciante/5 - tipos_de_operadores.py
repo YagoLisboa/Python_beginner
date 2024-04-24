35337573a89c31adb010d690b5bd87bf27d7b382
YagoLisboa <yago.lisboa@live.com>
2024-04-24 02:50:13 -0300

    tipos de operadores: aritmetica básica

---
+++ Iniciante/5 - tipos_de_operadores.py
@@ -0,0 +1,23 @@
+produto_1 = 50
+produto_2 = 30
+quantidade = 4
+
+# Operações aritméticas básicas
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * quantidade)
+print(produto_1 / quantidade)
+print(produto_2 ** quantidade)
+print(produto_2 // quantidade)
+print(produto_2 % quantidade)
+
+# Verificando a ordem de precedência de operações
+x = 10 + 5 * 4
+y = (10 + 5) * 4
+z = 10 / 2 + 25 * 4 - 2 ** 2
+n = 10 / (2 + 25) * (4 - 2) ** 2
+
+print(x)
+print(y)
+print(z)
+print(n)
